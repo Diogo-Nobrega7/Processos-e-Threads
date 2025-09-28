e5c6943337587e89e171f55d97aed25446b6535d
Luan Soares <luanmoon2005@gmail.com>
2025-09-27 23:39:47 -0300

    Update threads.c

--- threads.c
+++ threads.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <math.h>
 
 int **A, **B, **C;
 int n1, m1, n2, m2;
-int P; // número de threads
+int P; 
 
 typedef struct {
+    int id;
     int start, end;
 } ThreadData;
 
 void *multiplica(void *arg) {
     ThreadData *data = (ThreadData *)arg;
+
+    clock_t start_time = clock(); 
+
     for (int idx = data->start; idx < data->end; idx++) {
         int i = idx / m2;
         int j = idx % m2;
@@ -21,7 +26,35 @@ void *multiplica(void *arg) {
             C[i][j] += A[i][k] * B[k][j];
         }
     }
-    return NULL;
+
+    clock_t end_time = clock(); 
+
+  
+    char filename[64];
+    sprintf(filename, "ResultadoThreads_%d.txt", data->id);
+
+    FILE *fout = fopen(filename, "w");
+    if (!fout) {
+        perror("Erro ao criar arquivo de saída");
+        pthread_exit(NULL);
+    }
+
+ 
+    fprintf(fout, "%d %d\n", n1, m2);
+
+
+    for (int idx = data->start; idx < data->end; idx++) {
+        int i = idx / m2;
+        int j = idx % m2;
+        fprintf(fout, "%d %d %d\n", i, j, C[i][j]); 
+        
+    }
+
+    double tempo = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    fprintf(fout, "Tempo thread %d: %f segundos\n", data->id, tempo);
+
+    fclose(fout);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -30,13 +63,19 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    P = atoi(argv[1]);
+    P = atoi(argv[1]); 
 
     FILE *f1 = fopen("M1.txt", "r");
     FILE *f2 = fopen("M2.txt", "r");
 
+    if (!f1 || !f2) {
+        printf("Erro ao abrir arquivos M1.txt ou M2.txt\n");
+        return 1;
+    }
+
     fscanf(f1, "%d %d", &n1, &m1);
     fscanf(f2, "%d %d", &n2, &m2);
+
 
     A = malloc(n1 * sizeof(int *));
     B = malloc(n2 * sizeof(int *));
@@ -56,33 +95,32 @@ int main(int argc, char *argv[]) {
     fclose(f1);
     fclose(f2);
 
-    pthread_t threads[P];
-    ThreadData data[P];
     int total = n1 * m2;
-    int chunk = (total + P - 1) / P;
+    int num_threads = (total + P - 1) / P; 
 
-    clock_t start = clock();
-    for (int t = 0; t < P; t++) {
-        data[t].start = t * chunk;
-        data[t].end = (t + 1) * chunk;
+    pthread_t threads[num_threads];
+    ThreadData data[num_threads];
+
+    for (int t = 0; t < num_threads; t++) {
+        data[t].id = t;
+        data[t].start = t * P;
+        data[t].end = (t + 1) * P;
         if (data[t].end > total) data[t].end = total;
         pthread_create(&threads[t], NULL, multiplica, &data[t]);
     }
 
-    for (int t = 0; t < P; t++) pthread_join(threads[t], NULL);
-    clock_t end = clock();
-
-    FILE *fout = fopen("ResultadoThreads.txt", "w");
-    fprintf(fout, "%d %d\n", n1, m2);
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m2; j++) {
-            fprintf(fout, "%d ", C[i][j]);
-        }
-        fprintf(fout, "\n");
+    for (int t = 0; t < num_threads; t++) {
+        pthread_join(threads[t], NULL);
     }
-    fprintf(fout, "Tempo: %f segundos\n", (double)(end - start) / CLOCKS_PER_SEC);
-    fclose(fout);
 
-    printf("Resultado salvo em ResultadoThreads.txt\n");
+    printf("%d arquivos de saída gerados (ResultadoThreads_X.txt)\n", num_threads);
+
+    for (int i = 0; i < n1; i++) free(A[i]);
+    for (int i = 0; i < n2; i++) free(B[i]);
+    for (int i = 0; i < n1; i++) free(C[i]);
+    free(A); free(B); free(C);
+
     return 0;
 }
+
+
